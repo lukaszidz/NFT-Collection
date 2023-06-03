13e8cee9e2612a638f0f150c490e11f603a75606
Łukasz Idziak <lidziak@ah4r.com>
2023-06-03 15:31:19 +0200

    Initial commit with contracts and app

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x00b829b588e27b5cd20632e24F97D5b308B7d726";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
